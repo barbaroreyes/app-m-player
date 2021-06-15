1fe048b3197d742eb575399d4bd396c348e5b960
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-06-15 18:13:41 -0500

    added-link-categorie

--- src/components/Categorie.js
+++ src/components/Categorie.js
@@ -1,15 +1,18 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Categorie = () => {
-  return (
+  return (<>
+       <div>
+           <Link to='/'>Home</Link>
+       </div>
     <div className="categoria">
-       
         <div className="categoria-big-boz" >clip image to see</div>
      <div className="categoria-box" >titulo</div>
      <div className="categoria-box">Author</div>
      <div className="categoria-box">description</div>
      <div className="categoria-box">Time</div>
     </div>
+    </>
   )
 }
 
